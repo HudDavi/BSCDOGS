06903b9fa216ad0f9169f9982ef3c1ba4582bc9b
Hudson Davi <hudson.davi.cds@gmail.com>
2023-01-20 02:12:46 -0400

    V1.0.0

--- blog/Scripts/activate.bat
+++ blog/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hdavi\Documents\Projetos\Blog\Flask\BSC DOGS\PT\blog
+set VIRTUAL_ENV=C:\Users\hdavi\Documents\Projetos\Blog\Flask\BSC\BSCDOGS\blog
 
 if not defined PROMPT set PROMPT=$P$G
 
